6d734fe9e14d8497a75c561e6d58cf937ee4365d
J_C___ <jcsumlin@gmail.com>
2018-05-29 14:53:10 -0400

    initial push

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <?php
-
+$bearer = "";
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
@@ -12,7 +12,7 @@ curl_setopt_array($curl, array(
     CURLOPT_CUSTOMREQUEST => "GET",
     CURLOPT_HTTPHEADER => array(
         "Accept: application/json",
-        "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidGVzdDFAbWFpbC5jb20iLCJlbWFpbCI6InRlc3QxQG1haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJpc3MiOiJodHRwczovL3BheXNjYXBlLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1YjA1YTJlZGUxZmVlMDY2NzAwYzQ4N2IiLCJhdWQiOiJZVHd2Q2pBd2R4Znd1YnpNWGN3TE1DbENKZWZycEtwTCIsImlhdCI6MTUyNzYxMTg3MSwiZXhwIjoxNTI3Njk4MjcxfQ.ROHcPr43gFFIqsn8PPqwuEuXcHifI9r8BRrOYUdQUbw"
+        "Authorization: ".$bearer
     ),
 ));
 
@@ -50,7 +50,7 @@ curl_setopt_array($curl, [
     CURLOPT_CUSTOMREQUEST => "GET",
     CURLOPT_HTTPHEADER => [
         "Accept: application/json",
-        "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidGVzdDFAbWFpbC5jb20iLCJlbWFpbCI6InRlc3QxQG1haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJpc3MiOiJodHRwczovL3BheXNjYXBlLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1YjA1YTJlZGUxZmVlMDY2NzAwYzQ4N2IiLCJhdWQiOiJZVHd2Q2pBd2R4Znd1YnpNWGN3TE1DbENKZWZycEtwTCIsImlhdCI6MTUyNzYxMTg3MSwiZXhwIjoxNTI3Njk4MjcxfQ.ROHcPr43gFFIqsn8PPqwuEuXcHifI9r8BRrOYUdQUbw"
+        "Authorization: ".$bearer
     ],
 ]);
 
