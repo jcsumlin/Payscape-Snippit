3c27878bef1fcb7da6e0b39f9664d01c7bdf1f99
J_C___ <jcsumlin@gmail.com>
2018-05-30 13:15:37 -0400

    reformat

--- mainFunction.php
+++ mainFunction.php
@@ -1,53 +1,49 @@
 <?php
 
-function CallAPI($method = "GET", $bearer, $url, $id)
+function CallAPI($method, $bearer, $url, $id)
 {
-    if ($bearer == NULL || $url == NULL || $id == NULL) {
-        echo "One of the required parameters is not provided!";
+    $curl = curl_init();
+    if ($method == 'GET') {
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url . $id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "Accept: application/json",
+                "Authorization: $bearer"
+            ),
+        ));
+
     } else {
-        $curl = curl_init();
-        if ($method == 'GET') {
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url . $id,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_HTTPHEADER => array(
-                    "Accept: application/json",
-                    "Authorization: $bearer"
-                ),
-            ));
+        echo "Invalid Method";
+    }
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-        } else {
-            echo "Invalid Method";
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        if(strpos($url, 'program') !== false && json_decode($response, true)[0]['status'] == "Draft") {
+            echo "This Program is still a Draft!";
         }
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            if(strpos($url, 'program') !== false && json_decode($response, true)[0]['status'] == "Draft") {
-                echo "This Program is still a Draft!";
+        $result = json_decode($response, true)[0];
+        if (strpos($url, 'program') !== false) {
+            if (($result['price_range']['min'] == $result['price_range']['max']) && $result['price_range']['min'] == "0") {
+                $result['price'] = "Free!";
+
+            } elseif ($result['price_range']['min'] == $result['price_range']['max'])   {
+                $result['price'] = $result['price_range']['min'];
+            } else {
+                $result['price'] = $result['price_range']['min'] . " - " . $result['price_range']['max'];
             }
-
-            $result = json_decode($response, true)[0];
-            if (strpos($url, 'program') !== false) {
-                if (($result['price_range']['min'] == $result['price_range']['max']) && $result['price_range']['min'] == "0") {
-                    $result['price'] = "Free!";
-
-                } elseif ($result['price_range']['min'] == $result['price_range']['max'])   {
-                    $result['price'] = $result['price_range']['min'];
-                } else {
-                    $result['price'] = $result['price_range']['min'] . " - " . $result['price_range']['max'];
-                }
-            }
-
-            return $result;
         }
+
+        return $result;
     }
 
 }
