0d376cbcc9dcca02b0884793f761ce6db3c794ab
J_C___ <jcsumlin@gmail.com>
2018-05-30 11:45:08 -0400

    refactor

--- test.php
+++ test.php
@@ -1,37 +1,43 @@
 <?php
 
-$curl = curl_init();
+function CallAPI($method = "GET", $bearer, $url, $id)
+{
+    if ($bearer == NULL || $url == NULL || $id == NULL) {
+        echo "One of the required parameters is not provided!";
+    } else {
+        $curl = curl_init();
+        if ($method == 'GET') {
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url . $id,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "Accept: application/json",
+                    "Authorization: $bearer"
+                ),
+            ));
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => "http://api.intdev.registration.payscape.com/programs/46",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-        "Accept: application/json",
-        "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidGVzdDFAbWFpbC5jb20iLCJlbWFpbCI6InRlc3QxQG1haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJpc3MiOiJodHRwczovL3BheXNjYXBlLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1YjA1YTJlZGUxZmVlMDY2NzAwYzQ4N2IiLCJhdWQiOiJZVHd2Q2pBd2R4Znd1YnpNWGN3TE1DbENKZWZycEtwTCIsImlhdCI6MTUyNzYxMTg3MSwiZXhwIjoxNTI3Njk4MjcxfQ.ROHcPr43gFFIqsn8PPqwuEuXcHifI9r8BRrOYUdQUbw"
-    ),
-));
+        } else {
+            echo "Invalid Method";
+        }
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            if(strpos($url, 'program') !== false && json_decode($response, true)[0]['status'] == "Draft") {
+                echo "This Program is still a Draft!";
+            }
+            $result = json_decode($response, true)[0];
 
-curl_close($curl);
-
-
-
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    if(json_decode($response, true)[0]['status'] == "Draft") {
-        echo "Program is still a Draft!";
+            return $result;
+        }
     }
-    $program = $response;
-    echo $program;
 
 }
-
 ?>
